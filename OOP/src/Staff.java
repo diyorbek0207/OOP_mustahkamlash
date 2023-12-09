f4639f1b670b905c4ae4baee547b389a38d1800a
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-09 12:29:30 +0500

    add Staff

--- OOP/src/Staff.java
+++ OOP/src/Staff.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Staff {
+public class Staff extends Person{
+    private String school;
+    private int pay;
+
+    public Staff(String name, String address,String school,int pay) {
+        super(name, address);
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getPay() {
+        return pay;
+    }
+
+    public void setPay(int pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff [ Person["+getName()+","+getAddress()+"]"+","+getSchool()+","+getPay()+"]";
+    }
 }
